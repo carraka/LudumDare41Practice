1224025430b3c646039cfcfac224495bd623b898
Sheldon Zamora-Soon <sheldonzs@gmail.com>
2018-04-07 21:44:54 -0400

    Added tower placement!

---
+++ LudumDare41-Practice/Assets/Scripts/Sheep.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour {
+    public int HP;
+    public int moveSpeed;
+    public int damage;
+    public int attackSpeed;
+    public int armor;
+
+    Sheep(int HP, int moveSpeed, int damage, int attackSpeed, int armor = 0)
+    {
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
